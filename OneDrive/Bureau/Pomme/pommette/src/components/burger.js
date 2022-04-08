489f6c8df0e77d3dd7d5eb67b01f3ceac48db96d
Bilel <HE201666@students.ephec.be>
2022-04-08 16:26:05 +0200

    modif page burger

--- OneDrive/Bureau/Pomme/pommette/src/components/burger.js
+++ OneDrive/Bureau/Pomme/pommette/src/components/burger.js
@@ -7,8 +7,8 @@ class Burger extends Component{
   render(){
       return(
             <div>
-            <div class="">
-              <div className="menubord2">
+            <div class="col-md-4 wow fadeIn">
+              <div className="menubord3">
                 <a href="/burger" class="link-light">
                    <h3 class="mb50" className='popo'><span class="heading-font text-uppercase">Burgers</span></h3>
                 </a>
@@ -20,6 +20,16 @@ class Burger extends Component{
                 <p>Charolais beef 200gr, poivre concassés, cheddar, sauce au poivre, oignons frits, Black bun, salade & tomate. accompagné de frites</p>
                 <h4>Dikkenek <span class="theme-accent-color">20.00€</span></h4>
                 <p>Charolais beef 200gr, fromage maredsous, sauce dallas, oignons caramélisés, salade & tomate. accompagné de frites</p>
+                <h4>Black Pepper <span class="theme-accent-color">20.00€</span></h4>
+                <p>Charolais beef 200gr, poivre concassés, cheddar, sauce au poivre, oignons frits, Black bun, salade & tomate. accompagné de frites</p>
+                <h4>Black Pepper <span class="theme-accent-color">20.00€</span></h4>
+                <p>Charolais beef 200gr, poivre concassés, cheddar, sauce au poivre, oignons frits, Black bun, salade & tomate. accompagné de frites</p>
+                <h4>Black Pepper <span class="theme-accent-color">20.00€</span></h4>
+                <p>Charolais beef 200gr, poivre concassés, cheddar, sauce au poivre, oignons frits, Black bun, salade & tomate. accompagné de frites</p>
+                <h4>Black Pepper <span class="theme-accent-color">20.00€</span></h4>
+                <p>Charolais beef 200gr, poivre concassés, cheddar, sauce au poivre, oignons frits, Black bun, salade & tomate. accompagné de frites</p>
+                <h4>Black Pepper <span class="theme-accent-color">20.00€</span></h4>
+                <p>Charolais beef 200gr, poivre concassés, cheddar, sauce au poivre, oignons frits, Black bun, salade & tomate. accompagné de frites</p>
               </div>
             </div>
             </div>
